3e486ed5c170d08ac8e9127190cc4dfe184ffa29
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-04-26 20:03:50 +0100

    acquired react in codebase
    - Added React script tags
    - included boiler code in index.js

--- Getting-started -with-React/index.js
+++ Getting-started -with-React/index.js
@@ -1,1 +1,24 @@
-ReactDOM.render(<h1>First Render</h1>, document.getElementById(root));
+'use strict';
+
+const e = React.createElement;
+
+class likeButton extends React.Component {
+    constructor(props){
+        super(props)
+        this.liked = { liked: false};
+    }
+    render() {
+        if (this.state.liked) return `You're in love.`;
+    
+        return e(
+            'button',
+            {onClick: () => this.setState({liked: true})},
+            "Like"
+        );
+    }
+}
+
+const divContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(divContainer);
+
+root.render(e(likeButton));
