f687b9b6567e1097b436d94871c6cf975a2647ee
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-08-15 12:40:45 +0100

    add s to display

--- tenzies/src/Scoreboard.js
+++ tenzies/src/Scoreboard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Scoreboard ({timer, highScore}){
     return (
         <div className="scoreboard">
-            <h1 className="score">Best-Time: {highScore}</h1>
+            <h1 className="score">Best-Time: {highScore}s</h1>
             <h1 className="score">Time: {timer}s</h1>
         </div>
     )
