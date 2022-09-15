8228b800a6e69a311e6abd4f8bc1431c1bf3e64f
Timilehin Bakare <timmybaks2002@gmail.com>
2022-09-15 01:15:18 +0100

    Create function formatScore

--- tenzies/src/Scoreboard.js
+++ tenzies/src/Scoreboard.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 export default function Scoreboard ({timer, highScore}){
+
+    const formatScore = (score) =>{
+
+        return formattedScore
+    }
+    
     return (
+        
         <div className="scoreboard">
             <h1 className="score">Best-Time: {highScore}s</h1>
             <h1 className="score">Time: {timer}s</h1>
